299b8ce1a5fd3f06a098efca4d7609b100ef84db
Darling Rodriguez <darlingrodriguez@Darlings-MacBook-Pro.local>
2022-07-05 17:08:34 -0400

    did hard one

--- Hard/javasc.js
+++ Hard/javasc.js
@@ -1,26 +1,15 @@
+var Tom= [9, 8] //[Height, Mass]
+var Jerry= [10, 45] //[Height, Mass]
 
-function add(num1, num2) {
-    return num1 + num2;
-  }
-
-
-function subtract_1from2(num1, num2) {
-    return num1 - num2;
-  }
-
-function subtract_2from1(num1, num2) {
-    return num2 - num1;
+function BMICalc(hei, mass){
+  var bmi = mass / (hei * hei);
+  return bmi;
 }
 
+if (BMICalc(Tom[0], Tom[1]) > BMICalc(Jerry[0], Jerry[1])) {
+    var TomBMI = true;
+}else{
+    var TomBMI = false;
+}
 
-function Multiply(num1, num2) {
-    return num1 * num2;
-  }
-
-function divide_1by2(num1, num2) {
-    return (num1/num2);
-  }
-
-  function divide_2by1(num1, num2) {
-    return (num2/num1);
-  }
+console.log(`Is Toms BMI higher than Jerrys? ${TomBMI}`);
--- Medium/javas.js
+++ Medium/javas.js
@@ -1,10 +1,44 @@
-var name = window.prompt("Type anything: ");
+while (true) {
+  const num = parseInt(prompt("Enter Number: (-1 to stop)"));
 
-if (name.toUpperCase() === name) {
-    console.log('Yelling');
-  } else if(name.toLowerCase() === name){
-    console.log('Whispering');
+  if (num === -1){
+      break;
   }
-  else{
-    console.log('Talking Normal');
-  }
+  else if ( num === 1) {
+      window.alert(`The Month for ${num} is: January`);
+  } 
+  else if ( num === 2) {
+      window.alert(`The Month for ${num} is: February`);
+  } 
+  else if ( num === 3) {
+      window.alert(`The Month for ${num} is: March`);
+  } 
+  else if ( num === 4) {
+      window.alert(`The Month for ${num} is: April`);
+  } 
+  else if ( num === 5) {
+      window.alert(`The Month for ${num} is: May`);
+  } 
+  else if ( num === 6) {
+      window.alert(`The Month for ${num} is: June`);
+  } 
+  else if ( num === 7) {
+      window.alert(`The Month for ${num} is: July`);
+  } 
+  else if ( num === 8) {
+      window.alert(`The Month for ${num} is: August`);
+  } 
+  else if ( num === 9) {
+      window.alert(`The Month for ${num} is: September`);
+  } 
+  else if ( num === 10) {
+      window.alert(`The Month for ${num} is: October`);
+  } 
+  else if ( num === 11) {
+      window.alert(`The Month for ${num} is: November`);
+  } 
+  else if ( num === 12) {
+      window.alert(`The Month for ${num} is: December`);
+  } 
+}
+
