0e5bf14251341a538fc3b73f366cd819b68e574a
Darling Rodriguez <darlingrodriguez@Darlings-MacBook-Pro.local>
2022-07-05 20:28:07 -0400

    re-implemented hard

--- Hard/javasc.js
+++ Hard/javasc.js
@@ -1,12 +1,19 @@
-var Tom= [9, 8] //[Height, Mass]
-var Jerry= [10, 45] //[Height, Mass]
+const Tom = {
+  height: 9,
+  mass: 8,
+};
+
+const Jerry = {
+  height: 10,
+  mass: 45,
+};
 
 function BMICalc(hei, mass){
   var bmi = mass / (hei * hei);
   return bmi;
 }
 
-if (BMICalc(Tom[0], Tom[1]) > BMICalc(Jerry[0], Jerry[1])) {
+if (BMICalc(Tom.height, Tom.mass) > BMICalc(Jerry.height, Jerry.mass)) {
     var TomBMI = true;
 }else{
     var TomBMI = false;
